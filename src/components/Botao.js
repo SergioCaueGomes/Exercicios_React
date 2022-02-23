dbb6c2a3bd16572eebe269d030b7925fe826d6bd
Sergio <caue.sergio@gail.com>
2022-02-23 20:11:02 -0300

    Commit Botoes

--- src/components/Botao.js
+++ src/components/Botao.js
@@ -5,11 +5,17 @@ export default props => {
     function cadastrar() {
         console.log('Cadastrou');
     }
+    function atualizar() {
+        console.log('Atualizou');
+    }
+    function deletar() {
+        console.log('Deletou');
+    }
     return (
         <>
         <button className="btn" onClick={cadastrar}>Cadastrar</button>
-        <button className="btn" onClick={cadastrar}>Cadastrar</button>
-        <button className="btn" onClick={cadastrar}>Cadastrar</button>
+        <button className="btn" onClick={atualizar}>Atualizar</button>
+        <button className="btn" onClick={deletar}>Deletar</button>
         </>
     )
 }
