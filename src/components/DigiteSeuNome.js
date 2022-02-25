c90a5418ef78a1012f9253fe56168a8ad71a3941
Sergio <caue.sergio@gail.com>
2022-02-25 17:23:01 -0300

    correcao

--- src/components/DigiteSeuNome.js
+++ src/components/DigiteSeuNome.js
@@ -5,13 +5,27 @@ export function DigiteSeuNome(props) {
     const [nome, setNome] = useState('')
     const [email, setEmail] = useState('')
 
+    let cadastrar = () => {
+        console.log("Cadastrou")
+    }
     return (
+
         <>
-            <h2>{nome}</h2>
-            <h2>{email}</h2>
-            <input type="text" placeholder="Digite seu nome" value={nome} onChange={event => setNome(event.target.value)}/>
-            <input type="email" placeholder='Digite seu email' value={email} onChange={event => setEmail(event.target.value)}/>
+            <form>
+                <label>Nome:</label><br/>
+                <input type="text" placeholder="Digite seu nome" value={nome} onChange={event => setNome(event.target.value)}/><br></br>
+                <br/><label>Email:</label><br/>
+                <input type="email" placeholder='Digite seu email' value={email} onChange={event => setEmail(event.target.value)}/><br></br>
+                <br></br><button onClick={() => cadastrar()}>Cadastrar</button>
+            </form>
         </>
+
+        // <>
+        //     <h2>{nome}</h2>
+        //     <h2>{email}</h2>
+        //     <input type="text" placeholder="Digite seu nome" value={nome} onChange={event => setNome(event.target.value)}/>
+        //     <input type="email" placeholder='Digite seu email' value={email} onChange={event => setEmail(event.target.value)}/>
+        // </>
     );
 }
 
